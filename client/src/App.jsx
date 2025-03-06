64b9921bb44e5ed60a8b75b3456f25d0c2c19e97
hammadurrehman01 <hammadurrehman1954@gmail.com>
2025-03-06 12:11:40 +0500

    refactor App component and server setup for improved message handling and room management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,24 +3,24 @@ import React, { useEffect, useMemo, useState } from "react";
 import { io } from "socket.io-client";
 
 const App = () => {
-  const socket = useMemo(() => io("http://localhost:8000/"), [])
+  const socket = useMemo(() => io("http://localhost:8000/"), []);
 
+  const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
   const [room, setRoom] = useState("");
   const [socketID, setSocketID] = useState("");
-  const [messages, setMessages] = useState([]);
-  const [roomName, setRoomName] = useState([]);
+  const [roomName, setRoomName] = useState("");
 
   useEffect(() => {
     socket.on("connect", () => {
-      setSocketID(socket.id)
+      setSocketID(socket.id);
       console.log(`Connected ${socket.id}`);
     });
 
     socket.on("receive-message", (msg) => {
-      console.log(msg)
-      setMessages((messages) => [...messages, msg])
-    })
+      console.log(msg);
+      setMessages((messages) => [...messages, msg]);
+    });
 
     return () => {
       socket.disconnect();
@@ -29,14 +29,15 @@ const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    socket.emit('message', { message, room });
-    setMessage("")
-    setRoom("")
-  }
+    socket.emit("message", { message, room });
+    setMessage("");
+  };
 
-  const joinRoomHandler = () => {
-
-  }
+  const joinRoomHandler = (e) => {
+    e.preventDefault();
+    socket.emit("join-room", roomName);
+    setRoomName("");
+  };
 
   return (
     <Container maxWidth="sm">
@@ -44,37 +45,73 @@ const App = () => {
         Welcome to the chat app
       </Typography>
 
-
-      <Typography variant="h6" component="div" gutterBottom>{socketID}</Typography>
+      {/* <Typography variant="h6" component="div" gutterBottom>
+        {socketID}
+      </Typography> */}
 
       <form onSubmit={joinRoomHandler}>
         <h5>Join Room</h5>
-        <TextField
-          value={roomName}
-          onChange={(e) => e.target.value}
-          variant="outlined"
-          label="Room Name"
-        />
-        <Button type="submit" variant="contained" color="primary">Join</Button>
+        <Stack
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            alignItems: "center",
+            gap: "8px",
+          }}
+        >
+          <TextField
+            value={roomName}
+            onChange={(e) => setRoomName(e.target.value)}
+            variant="outlined"
+            label="Room Name"
+            sx={{ width: { xs: "100%", md: "50%" } }}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            sx={{ width: { xs: "100%", md: "15%" } }}
+          >
+            Join
+          </Button>
+        </Stack>
       </form>
 
-
-      <form onSubmit={handleSubmit}>
-        <TextField
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          id="outlined-basic"
-          label="Message"
-          variant="outlined"
-        />
-        <TextField
-          value={room}
-          onChange={(e) => setRoom(e.target.value)}
-          id="outlined-basic"
-          label="Room"
-          variant="outlined"
-        />
-        <Button type="submit" variant="contained" color="primary">Send</Button>
+      <form onSubmit={handleSubmit} style={{ marginTop: "10px" }}>
+        <h5>Send Message</h5>
+        <Stack
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            alignItems: "center",
+            gap: "8px",
+          }}
+        >
+          <TextField
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            id="outlined-basic"
+            label="Message"
+            variant="outlined"
+            sx={{ width: { xs: "100%", md: "50%" } }}
+          />
+          <TextField
+            value={room}
+            onChange={(e) => setRoom(e.target.value)}
+            id="outlined-basic"
+            label="Room"
+            variant="outlined"
+            sx={{ width: { xs: "100%", md: "50%" } }}
+          />
+        </Stack>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{ marginTop: "4px", width: { xs: "100%", md: "15%" } }}
+        >
+          Send
+        </Button>
       </form>
 
       <Stack>
--- server/app.js
+++ server/app.js
@@ -5,41 +5,47 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
-    credentials: true
-
+    credentials: true,
   },
 });
 
-io.on("connection", (socket) => {
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
+io.use((socket, next) => {
+  if (true) next();
+});
+
+io.on("connection", (socket) => {
   socket.on("message", ({ message, room }) => {
-    console.log("msg: ", { message, room })
+    console.log("msg: ", { message, room });
 
     // // This will send the message to all users except the sender
     // socket.broadcast.emit("receive-message", msg)
 
     // // This is send the message to the particular user which we pass the id
-    io.to(room).emit("receive-message", message)
+    io.to(room).emit("receive-message", message);
+  });
 
-  })
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined the room ${room}`);
+  });
 
   socket.on("disconnect", () => {
     console.log(`User ${socket.id} has been disconnected`);
-  })
+  });
 });
 
 const PORT = 8000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
 server.listen(PORT, () => {
   console.log(`Server is listening to the port ${PORT}`);
