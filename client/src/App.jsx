e0a9cfbf8429d96911cd4bb0c31bdc36eeec49ea
hammadurrehman01 <hammadurrehman@techlabs.cc>
2025-03-06 08:07:40 +0500

    ``` implement user authentication with JWT and session management ```

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,87 @@
-import { Container, TextField, Typography } from "@mui/material";
-import React, { useEffect } from "react";
+import { Button, Container, Stack, TextField, Typography } from "@mui/material";
+import React, { useEffect, useMemo, useState } from "react";
 import { io } from "socket.io-client";
 
 const App = () => {
-  const socket = io("http://localhost:8000/");
+  const socket = useMemo(() => io("http://localhost:8000/"), [])
+
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [socketID, setSocketID] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [roomName, setRoomName] = useState([]);
 
   useEffect(() => {
     socket.on("connect", () => {
+      setSocketID(socket.id)
       console.log(`Connected ${socket.id}`);
     });
 
-    socket.on("Hello", (msg) => {
-      console.log(msg);
-    });
+    socket.on("receive-message", (msg) => {
+      console.log(msg)
+      setMessages((messages) => [...messages, msg])
+    })
 
     return () => {
       socket.disconnect();
     };
   }, []);
-const handleSubmit = (e) => {
-  
-}
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit('message', { message, room });
+    setMessage("")
+    setRoom("")
+  }
+
+  const joinRoomHandler = () => {
+
+  }
+
   return (
     <Container maxWidth="sm">
       <Typography variant="h1" component="div" gutterBottom>
         Welcome to the chat app
       </Typography>
 
-      <form action="">
+
+      <Typography variant="h6" component="div" gutterBottom>{socketID}</Typography>
+
+      <form onSubmit={joinRoomHandler}>
+        <h5>Join Room</h5>
         <TextField
-          id="outlined-basic"
-          label="Outlined"
+          value={roomName}
+          onChange={(e) => e.target.value}
           variant="outlined"
-        ></TextField>
-        <Button variant="contained" color="primary">Send</Button>
+          label="Room Name"
+        />
+        <Button type="submit" variant="contained" color="primary">Join</Button>
       </form>
+
+
+      <form onSubmit={handleSubmit}>
+        <TextField
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          id="outlined-basic"
+          label="Message"
+          variant="outlined"
+        />
+        <TextField
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          id="outlined-basic"
+          label="Room"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">Send</Button>
+      </form>
+
+      <Stack>
+        {messages.map((m, i) => (
+          <Typography key={i}>{m}</Typography>
+        ))}
+      </Stack>
     </Container>
   );
 };
--- server/app.js
+++ server/app.js
@@ -18,16 +18,24 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
+
+  socket.on("message", ({ message, room }) => {
+    console.log("msg: ", { message, room })
+
+    // // This will send the message to all users except the sender
+    // socket.broadcast.emit("receive-message", msg)
+
+    // // This is send the message to the particular user which we pass the id
+    io.to(room).emit("receive-message", message)
+
+  })
+
   socket.on("disconnect", () => {
     console.log(`User ${socket.id} has been disconnected`);
-    
   })
 });
 
-
-
 const PORT = 8000;
-
 
 app.get("/", (req, res) => {
   res.send("Hello World");
